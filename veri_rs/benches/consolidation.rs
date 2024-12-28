f0fe1a73bbbacd196e972acc0a9154fc5f5d3830
Yanpei Guo <2847399255@qq.com>
2024-12-28 16:51:58 +0800

    add vss benchmark

--- veri_rs/benches/consolidation.rs
+++ veri_rs/benches/consolidation.rs
@@ -88,7 +88,49 @@ fn das() {
     }
 }
 
+fn vss() {
+    let mut rng = thread_rng();
+    let mut wtr = Writer::from_path("vss_consolidation.csv").unwrap();
+    wtr.write_record(&["log_blob_size", "encode_time", "prover_time", "proof_size"])
+        .unwrap();
+    for log_blob_size in 16..21 {
+        let step = 3;
+        let log_layer_num = 1;
+        let code_rate = 1;
+        let prover = VeriRsProver::setup(log_blob_size, log_layer_num, code_rate, step);
+        let data = (0..(1 << log_blob_size))
+            .map(|_| <Fr as UniformRand>::rand(&mut rng))
+            .collect::<Vec<_>>();
+        let now = Instant::now();
+        for _ in 0..9 {
+            let _ = prover.encode(data.clone());
+        }
+        let codewords = prover.encode(data.clone());
+        let encode_time = now.elapsed().as_micros() as usize / 10;
+        let now = Instant::now();
+        for _ in 0..9 {
+            let _ = prover.prove(data.clone(), codewords.clone());
+        }
+        let proofs = prover.prove(data, codewords);
+        let prover_time = now.elapsed().as_micros() as usize / 10;
+        let symbol = proofs.n_th_replica(103);
+        let proof_size = symbol.proof_size();
+        wtr.write_record(
+            &[log_blob_size, encode_time, prover_time, proof_size].map(|x| x.to_string()),
+        )
+        .unwrap();
+        let verifier = VeriRsVerifier::setup(
+            103,
+            step,
+            log_blob_size - log_layer_num + code_rate,
+            code_rate,
+        );
+        assert!(verifier.verify(symbol));
+    }
+}
+
 fn main() {
-    vid();
-    das();
+    // vid();
+    // das();
+    vss();
 }
